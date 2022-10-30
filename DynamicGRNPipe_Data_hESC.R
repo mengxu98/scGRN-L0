48c0eba0be5d463f7420a3e1d2b0c6993e519307
mengxu98 <956854931@qq.com>
2022-10-30 17:14:31 +0800

    Updata

--- DynamicGRNPipe_Data_hESC.R
+++ DynamicGRNPipe_Data_hESC.R
@@ -12,7 +12,7 @@ CellInfor <- data.frame(
   sampleType = "hESC"
 )
 
-CellInfor.trajectory <- cbind.data.frame(CellInfor, PseTime.Lineage1=slingshot$PseudoTime)
+CellInfor.trajectory <- cbind.data.frame(CellInfor, PseTime.Lineage1 = slingshot$PseudoTime)
 head(CellInfor.trajectory)
 # ====2.slinding windows based on pseudotime and anotation of cells====
 source("DynamicGRNPipe_2.CellWindowing.R")
@@ -54,12 +54,14 @@ C5_C61_cross <- densityintersections(
   filename = "../scGRN-L0_output/all.png"
 )
 # Manually select the intersection point according to the density plot
-binpoint <- c(0, 
-              max(cellInfor$PseTime.Lineage1)/5, 
-              max(cellInfor$PseTime.Lineage1)*2/5, 
-              max(cellInfor$PseTime.Lineage1)*3/5, 
-              max(cellInfor$PseTime.Lineage1)*4/5, 
-              max(cellInfor$PseTime.Lineage1))
+binpoint <- c(
+  0,
+  max(cellInfor$PseTime.Lineage1) / 5,
+  max(cellInfor$PseTime.Lineage1) * 2 / 5,
+  max(cellInfor$PseTime.Lineage1) * 3 / 5,
+  max(cellInfor$PseTime.Lineage1) * 4 / 5,
+  max(cellInfor$PseTime.Lineage1)
+)
 # Two consecutive bins of cells as a window
 # extract cells for each window
 t1 <- cut(cellInfor$PseTime.Lineage1, binpoint)
@@ -73,14 +75,25 @@ Windows1 <- list(W1 = W1, W2 = W2, W3 = W3, W4 = W4)
 
 
 cellTypesList <- unique(cellTypes$Time)
-dataCellType <- data[tfs$TF[1:500], grep(cellTypesList[1], colnames(data))] %>% na.omit()
-L0REG_L0 <- L0REG(
-  matrix = dataCellType,
-  # regulators = tfs$TF,
-  # targets = colnames(data_GENIE3),
-  penalty = "L0"
-)
-write.table(L0REG_L0,
+dataCellTypes <- data[tfs$TF[1:500], ] %>% na.omit()
+
+for (i in 1:length(cellTypesList)) {
+  dataCellType <- data[tfs$TF[1:500], grep(cellTypesList[i], colnames(data))] %>% na.omit()
+  L0REG_L0 <- L0REG(
+    matrix = dataCellType,
+    # regulators = tfs$TF,
+    # targets = colnames(data_GENIE3),
+    penalty = "L0"
+  )
+  if (i == 1) {
+    L0REG_L0_list <- L0REG_L0
+  } else {
+    L0REG_L0_list$weight <- L0REG_L0_list$weight + L0REG_L0$weight
+  }
+}
+L0REG_L0_list <- L0REG_L0_list[order(L0REG_L0_list$weight,decreasing = TRUE),]
+
+write.table(L0REG_L0_list,
   paste0("../scGRN-L0_output/output_scRNA-Seq/output_L0GRN.txt"),
   sep = "\t",
   quote = F,
@@ -107,7 +120,7 @@ AUROC_L0REG_L0_N
 
 
 library(ppcor)
-inputExpr <- dataCellType
+inputExpr <- dataCellTypes
 geneNames <- rownames(inputExpr)
 rownames(inputExpr) <- c(geneNames)
 pcorResults <- pcor(x = t(as.matrix(inputExpr)), method = "spearman")
@@ -124,10 +137,10 @@ write.table(outDF,
   row.names = F,
   col.names = F
 )
-ground_truth_m(
+ground_truth_h(
   intput = paste0("../scGRN-L0_output/output_scRNA-Seq/GRN_PPCOR.txt"),
   output = "../scGRN-L0_output/output_scRNA-Seq/",
-  dataset_dir = mnetwork_data_dir,
+  dataset_dir = hnetwork_data_dir,
   database = "All"
 )
 evaluationObject <- prepareEval(paste0("../scGRN-L0_output/output_scRNA-Seq/GRN_PPCOR.txt"),
@@ -137,3 +150,49 @@ evaluationObject <- prepareEval(paste0("../scGRN-L0_output/output_scRNA-Seq/GRN_
 AUROC_PPCOR <- calcAUROC(evaluationObject)
 AUPRC_PPCOR <- calcAUPR(evaluationObject)
 AUROC_PPCOR
+
+library(LEAP)
+geneNames <- rownames(inputExpr)
+rownames(inputExpr) <- c()
+# Run LEAP's compute Max. Absolute Correlation
+# MAC_cutoff is set to zero to get a score for all TFs
+# max_lag_prop is set to the max. recommended value from the paper's supplementary file
+# Link to paper: https://academic.oup.com/bioinformatics/article/33/5/764/2557687
+MAC_results <- MAC_counter(
+  data = inputExpr, # max_lag_prop=maxLag,
+  MAC_cutoff = 0,
+  file_name = "temp", lag_matrix = FALSE, symmetric = FALSE
+)
+Gene1 <- geneNames[MAC_results[, "Row gene index"]]
+Gene2 <- geneNames[MAC_results[, "Column gene index"]]
+Score <- MAC_results[, "Correlation"]
+outDF <- data.frame(Gene1, Gene2, Score)
+write.table(outDF, paste0("../scGRN-L0_output/output_scRNA-Seq/GRN_LEAP.txt"),
+  sep = "\t",
+  quote = F,
+  row.names = F,
+  col.names = F
+)
+evaluationObject <- prepareEval(paste0("../scGRN-L0_output/output_scRNA-Seq/GRN_LEAP.txt"),
+  paste0(paste0("../scGRN-L0_output/output_scRNA-Seq/ground_truth.tsv")),
+  totalPredictionsAccepted = 100000
+)
+AUROC_LEAP <- calcAUROC(evaluationObject)
+AUPRC_LEAP <- calcAUPR(evaluationObject)
+AUROC_LEAP
+
+library(GENIE3)
+weightMat <- GENIE3(
+  exprMatrix = as.matrix(dataCellTypes),
+  nCores = 32
+)
+weightdf <- getLinkList(weightMat)
+names(weightdf) <- c("regulatoryGene", "targetGene", "weight")
+write.table(weightdf, file = paste0("../scGRN-L0_output/output_scRNA-Seq/GRN_GENIE3.txt"), row.names = FALSE, col.names = FALSE, sep = "\t", quote = FALSE)
+evaluationObject <- prepareEval(paste0("../scGRN-L0_output/output_scRNA-Seq/GRN_GENIE3.txt"),
+  paste0(paste0("../scGRN-L0_output/output_scRNA-Seq/ground_truth.tsv")),
+  totalPredictionsAccepted = 100000
+)
+AUROC_GENIE3 <- calcAUROC(evaluationObject)
+AUPRC_GENIE3 <- calcAUPR(evaluationObject)
+AUROC_GENIE3

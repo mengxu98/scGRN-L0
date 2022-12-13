c8b9049ebe8bbdaf50d43647bae56911b055fd40
MengXu <956854931@qq.com>
2022-12-13 12:31:44 +0800

    Update MetaCellGRN.R

--- MetaCellGRN.R
+++ MetaCellGRN.R
@@ -1,20 +1,52 @@
 
 
 output <- "../scGRN-L0_output/"
-sc_T_cell <- sc[which(sc$CellType == "T_cell"), ]
 table(sc$CellType)
 sc_T_cell= sc[, Idents(sc) %in% c( "T_cell")]
 table(MC.seurat$CellType)
 mc_T_cell= MC.seurat[, Idents(MC.seurat) %in% c( "T_cell")]
 
+matrix_sc <- as.data.frame(sc_T_cell@assays$RNA@scale.data)
+head(matrix_sc[1:5,1:5])
+max(matrix_sc)
+L0Dynamic <- L0REG(
+  matrix = matrix_sc,
+  regulators = hvg[1:200],
+  targets = hvg[1:200],
+  penalty = "L0"
+)
+max(L0Dynamic$weight)
+write.table(L0Dynamic,
+            paste0(output, "GRN_L0.txt"),
+            sep = "\t",
+            quote = F,
+            row.names = F,
+            col.names = F
+)
+
+ground_truth_h(
+  intput = paste0(output, "GRN_L0.txt"),
+  output = output,
+  dataset_dir = hnetwork_data_dir,
+  database = "STRING"
+)
+
+evaluationObject <- prepareEval(paste0(output, "GRN_L0.txt"),
+                                paste0(paste0(output, "ground_truth.tsv")),
+                                totalPredictionsAccepted = 100000)
+AUROC_L0_sc <- calcAUROC(evaluationObject)
+AUPRC_L0_sc <- calcAUPR(evaluationObject)
+AUROC_L0_sc
+AUPRC_L0_sc
+
+
 matrix <- as.data.frame(mc_T_cell@assays$RNA@scale.data)
-matrix <- as.data.frame(sc_T_cell@assays$RNA@scale.data)
 head(matrix[1:5,1:5])
 max(matrix)
 L0Dynamic <- L0REG(
   matrix = matrix,
-  regulators = hvg,
-  targets = hvg,
+  regulators = hvg[1:200],
+  targets = hvg[1:200],
   penalty = "L0"
 )
 max(L0Dynamic$weight)

9e9bd7efead1f38c847cd1d0e0b87d37f5964b6b
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-01-11 15:40:14 +0800

    Update Function-L0REG.R

--- Function-L0REG.R
+++ Function-L0REG.R
@@ -203,6 +203,22 @@ compute.gene.rank <- function(weightdf, directedGraph = FALSE) {
   return(pageRank)
 }
 
+#' Title
+#'
+#' @param grn
+#'
+#' @return
+#' @export
+#'
+#' @examples
+net.format <- function(grn){
+  colnames(grn) <- c("TF","TG","weight")
+  grn$weight <- as.numeric(grn$weight)
+  grn$interaction <- "activation"
+  grn$interaction[grn$weight < 0] <- "repression"
+  return(grn)
+}
+
 # --------------------------------------------------
 if (F) {
   wghts <- wghts / max(wghts)
@@ -223,7 +239,57 @@ if (F) {
   colnames(weightdf) <- colnames(matrix)
 }
 
+
 # Plot function --------------------------------------------------
+#' Plot of dynamic networks
+#'
+#'
+#' @param grn
+#' @param tfs tfs
+#' @param only_TFs plot only TF network
+#' 
+#' @return 
+#' 
+#' @export
+#'
+plot.dynamic.network <- function(weightdf, tfs = NULL, onlyTFs = TRUE, order = NULL, thresh = NULL){
+  library("ggnetwork")
+  df <- net.format(weightdf)
+  net <- graph_from_data_frame(df[, c("TF", "TG", "interaction")], directed=FALSE)
+  #tfnet<-ggnetwork(net,layout="fruchtermanreingold",cell.jitter=0)
+  layout <- layout_with_fr(net)
+  rownames(layout) <- V(net)$name
+  layout_ordered <- layout[V(net)$name,]
+  tfnet <- ggnetwork(net, layout = layout_ordered, cell.jitter=0)
+  tfnet$is_regulator <- as.character(tfnet$name %in% tfs)
+  cols<-c("activation" = "blue", "repression" = "red")
+  g<-ggplot()+
+    geom_edges(data=tfnet,
+               aes(x=x, y=y, xend=xend, yend=yend, color=interaction),
+               size=0.75,
+               curvature=0.1,
+               alpha=.6)+
+    geom_nodes(data=tfnet,
+               aes(x=x, y=y, xend=xend, yend=yend),
+               color="darkgray",
+               size=6,
+               alpha=.5)+
+    geom_nodes(data=tfnet[tfnet$is_regulator=="TRUE",],
+               aes(x=x, y=y, xend=xend, yend=yend),
+               color="#8C4985",
+               size=6,
+               alpha=.8)+
+    #geom_nodelabel_repel(data=tfnet,aes(x=x, y=y, label=vertex.names),size=6, color="#8856a7")+
+    scale_color_manual(values=cols)+
+    geom_nodelabel_repel(data=tfnet,
+                         aes(x=x, y=y, label=name),
+                         size=2.5,
+                         color="#5A8BAD")+
+    theme_blank() #+
+  #ggtitle(names(grn)[i])
+  g <- g + theme(legend.position="none")
+}
+
 L0Plot <- function(data, plotType = NULL) {
   if (is.null(plotType)) {
     plotType <- boxplot
